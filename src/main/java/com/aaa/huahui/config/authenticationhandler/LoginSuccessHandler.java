ed9964e54e4e81eac7e474164e2290adfe13f1c6
dewittt <dewittt@163.com>
2019-10-15 19:13:48 +0800

    add  staff role

--- src/main/java/com/aaa/huahui/config/authenticationhandler/LoginSuccessHandler.java
+++ src/main/java/com/aaa/huahui/config/authenticationhandler/LoginSuccessHandler.java
@@ -14,8 +14,6 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
