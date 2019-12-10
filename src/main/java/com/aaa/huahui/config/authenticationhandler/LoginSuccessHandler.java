f61693bba4f223a9f8fa010d754f927ea3fb275c
dewittt <dewittt@163.com>
2019-12-10 20:32:48 +0800

    add login shopid of brand authority

--- src/main/java/com/aaa/huahui/config/authenticationhandler/LoginSuccessHandler.java
+++ src/main/java/com/aaa/huahui/config/authenticationhandler/LoginSuccessHandler.java
@@ -1,32 +1,39 @@
 package com.aaa.huahui.config.authenticationhandler;
 
 import com.aaa.huahui.model.User;
+import com.aaa.huahui.service.ShopService;
 import com.aaa.huahui.utils.ResponseGenerate;
 import com.aaa.huahui.utils.ResponseUtil;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Iterator;
 
 
 @Component
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Autowired
+    ShopService shopService;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException, ServletException {
 
         Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
 
+        User user = (User) authentication.getPrincipal();
         JSONObject responseJson = null;
         if (authorities.size() == 0) {
             responseJson = ResponseGenerate.genFailResponse(1, "用户名或密码错误");
@@ -37,7 +44,13 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 
             String authority = grantedAuthority.getAuthority();
             authority = authority.substring(5).toLowerCase();
-
+            JSONArray array=null;
+            if (authority.equals("brand")){
+                array = new JSONArray();
+                ArrayList<Integer> list = shopService.selectAllShopId(user.getId());
+                list.forEach(array::add);
+                j.put("shopid",array);
+            }
             j.put("role", authority);
             responseJson = ResponseGenerate.genSuccessResponse(j);
         }
