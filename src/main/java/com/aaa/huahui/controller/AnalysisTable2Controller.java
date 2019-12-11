43944f6452dc922aab6d0968a9cc5103f4c996a4
dewittt <dewittt@163.com>
2019-12-11 16:54:19 +0800

    clear import

--- src/main/java/com/aaa/huahui/controller/AnalysisTable2Controller.java
+++ src/main/java/com/aaa/huahui/controller/AnalysisTable2Controller.java
@@ -1,15 +1,6 @@
 package com.aaa.huahui.controller;
 
-import com.aaa.huahui.config.ROLE;
-import com.aaa.huahui.model.User;
-import com.aaa.huahui.repository.ShopRepository;
-import com.aaa.huahui.repository.StaffRepository;
 import com.aaa.huahui.service.AnalysisTable2Service;
-import com.aaa.huahui.service.AnalysisTableService;
-import com.aaa.huahui.utils.DateUtils;
-import com.aaa.huahui.utils.ResponseGenerate;
-import com.aaa.huahui.vo.CustomerHandsVO;
-import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -19,11 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/analysis")
